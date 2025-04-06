54403f7e94677ba3dbf8b6a230b738b1ad6a0dc7
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-06 11:35:05 +0300

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,8 +1,15 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge, ReadOnly, NextTimeStep
+from cocotb_bus.drivers import BusDriver
 
 
 @cocotb.test()
 async def dut_test(dut):
+    a=(0,0,1,1)
+    b=(0,1,0,1)
+    y=(0,1,1,1)
+    for in range(4):
+        dut.a.value=a[i]
+        dut.b.value=b[i]
+        assert dut.y.value==y[i], "TEST FAILED"
     
-    assert 0, "Test not Implemented"
