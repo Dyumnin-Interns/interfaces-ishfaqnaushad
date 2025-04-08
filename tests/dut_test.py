79891c6a2dde22339fce5b2500a1ee3ff590bc59
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-09 00:43:23 +0300

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -12,20 +12,30 @@ async def dut_test(dut):
     a=(0,0,1,1)
     b=(0,1,0,1)
     expected_value=(0,1,1,1)
+    dut.write_en.value=0
+    dut.read_en.value=0
     
     
     for i in range(4):
+        await RisingEdge(dut.CLK)
+        await NextTimeStep()
+        dut.read_en.value=0
         dut.write_address.value=3
         dut.write_data.value=a[i]
         dut.write_en.value=1
         await RisingEdge(dut.CLK)
+        await NextTimeStep()
+        dut.writ_en.value=0
         await NextTimeStep()
         dut.write_address.value=4
         dut.write_data.value=b[i]
         dut.write_en.value=1
         await RisingEdge(dut.CLK)
         await NextTimeStep()
+        dut.write_en.value=0
+        await NextTimeStep()
         dut.write_address.value=5
         await ReadOnly()
+        dut.read_en.value=1
         assert dut.read_data.value==expected_value[i], "Test Failed"
 
