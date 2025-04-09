a85f104e8a8c91dfce6152fe15d7ee6b1092d947
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-09 22:46:00 +0300

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -24,11 +24,9 @@ async def dut_test(dut):
     dut.RST_N.value=1
     await Timer(1,'ns')
     adrv=WriteDriver(dut,'write',dut.CLK,4)
-    ReadDriver(dut,'read',dut.CLK,sb_fn,3)
     for i in range(4):
         adrv.append(a[i])
-        while len(expected_value)>0:
-            await Timer(2,'ns')
+        await Timer(2,'ns')
             
     
         
