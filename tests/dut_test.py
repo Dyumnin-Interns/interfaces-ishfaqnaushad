80e4cb924a7ca05bfba447c0957dda6453a03919
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-10 14:05:26 +0300

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -15,70 +15,33 @@ async def dut_test(dut):
     global expected_value
     a=(0,0,1,1)
     b=(0,1,0,1)
-    expected_value=[0,1,1,1]
-    dut.RST_N.value=1
-    await Timer(1, 'ns')
-    dut.RST_N.value=0
-    await Timer(1, 'ns')
-    await RisingEdge(dut.CLK)
-    dut.RST_N.value=1
+    expected_value=(0,1,1,1)
+    dut.write_en.value=0
+    dut.read_en.value=0
     await Timer(1,'ns')
-    adrv=WriteDriver(dut,'write',dut.CLK, 4)
-    adrv.append(a[0])
-    await Timer(6,'ns')
-    bdrv=WriteDriver(dut,'write',dut.CLK, 5)
-    bdrv.append(b[0])
-    await Timer(6,'ns')
-    ReadDriver(dut,'read',dut.CLK,sb_fn,3)
-    await Timer(1,'ns')
-    adrv=WriteDriver(dut,'write',dut.CLK, 4)
-    adrv.append(a[1])
-    await Timer(6,'ns')
-    bdrv=WriteDriver(dut,'write',dut.CLK, 5)
-    bdrv.append(b[1])
-    await Timer(6,'ns')
-    ReadDriver(dut,'read',dut.CLK,sb_fn,3)
-        
-class WriteDriver(BusDriver):
-    _signals=['address', 'rdy', 'en', 'data']
-    def __init__(self, dut, name, clk, address):
-        BusDriver.__init__(self, dut, name, clk)
-        self.bus.en.value=0
-        self.clk=clk
-        self.bus.address.value=address
-        
-    async def _driver_send(self,value,sync=True):
-        if self.bus.rdy.value!=1:
-            await RisingEdge(self.bus.rdy)
-        self.bus.en.value=1
-        self.bus.data.value=value
+    for i in range(4):
+        dut.write_address.value=4
+        dut.write_en.value=1
+        dut.write_data.value=a[i]
+        await Timer(1,'ns')
+        await RisingEdge(dut.CLK)
+        dut.write_en.value=0
+        await NextTimeStep()
+        dut.write_address.value=5
+        dut.write_en.value=1
+        dut.write_data.value=b[i]
+        await Timer(1,'ns')
+        await RisingEdge(dut.CLK)
+        dut.write_en.value=0
+        dut.read_en.value=1
+        dut.read_address.value=3
         await ReadOnly()
-        await RisingEdge(self.clk)
-        await NextTimeStep()
-        self.bus.en.value=0
-        await NextTimeStep()
+        assert dut.read_data.value==expected_value[i], f"Test Failed,A={a[i]},B={b[i]},actual={dut.read_data.value.integer},expected={expected_value[i]}"
         
-class ReadDriver(BusDriver):
-    _signals=['address', 'rdy', 'en', 'data']
-    def __init__(self, dut, name, clk, sb_callback,address):
-        BusDriver.__init__(self, dut, name, clk)
-        self.bus.en.value=0
-        self.clk=clk
-        self.callback=sb_callback
-        self.bus.address.value=address
-        self.append(0)
-    
-    async def _driver_send(self,value,sync=True):
-        while True:
-            if self.bus.rdy.value!=1:
-                await RisingEdge(self.bus.rdy)
-            self.bus.en.value=1
-            await ReadOnly()
-            self.callback(self.bus.data.value)
-            await NextTimeStep()
-            await RisingEdge(self.clk)
-            await NextTimeStep()
-            self.bus.en.value=0
+        
+
+        
+
  
 
             
