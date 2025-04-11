0fbe106d7614d9011b9ce432d61a3fcee8e1550f
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-11 18:07:26 +0300

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -27,7 +27,9 @@ async def dut_test(dut):
         await Timer(6,'ns')
         bdrv=WriteDriver(dut,'write',dut.CLK,5)
         bdrv.append(b[i])
-        await Timer(3,'ns')
+        dut.read.address=1
+        if dut.read_data.value!=1:
+            await RisingEdge(dut.read_data)
         ReadDriver(dut,'read',dut.CLK,sb_fn)
         await Timer(6,'ns')
         
