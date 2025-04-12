39d5c8c3a3c2ede8f37af5b4bf9218daf99661a4
ishfaqnaushad <ishfaqahmednaushad@gmail.com>
2025-04-13 00:48:02 +0300

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -133,7 +133,7 @@ class IO_Monitor(BusMonitor):
         while True:
             await fallingedge
             await rdonly
-            txn=(self.bus.en.value<<1)|self.bus.rdy.value
+            txn=(self.bus.en.value << 1) | self.bus.rdy.value
             self._recv({'previous':prev,'current':phases[txn]})
             prev=phases[txn]
         
